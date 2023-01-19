e22858342c351c7d850e2522b0e3fab5edc8a126
alonsoandrade <aandrade@freepik.com>
2023-01-19 20:35:53 +0100

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ import requests
 import streamlit as st
 
 # get profile from query params
-query_params = st.experimental_get_query_params()
+# query_params = st.experimental_get_query_params()
 
-profile = query_params['id_alumno'][0] if 'id_alumno' in query_params else 'A0'
+# profile = query_params['id_alumno'][0] if 'id_alumno' in query_params else 'A0'
 
 # add credentials to the account
 keyfile_dict = json.loads(st.secrets["GCP_SERVICE_ACCOUNT"])
@@ -22,181 +22,67 @@ creds = ServiceAccountCredentials.from_json_keyfile_dict(keyfile_dict, scope)
 client = gspread.authorize(creds)
 
 # get the instance of the Spreadsheet
-database = client.open('josemanuel_app_database')
+database = client.open('club_fiction')
 
-# students
-students_table = database.get_worksheet(0)
-students_df = pd.DataFrame(students_table.get_all_records()).iloc[: , :6]
+# movies
+movies_table = database.get_worksheet(1)
+movies_df = pd.DataFrame(movies_table.get_all_records())
+st.set_page_config(layout="wide")
+st.markdown('## Club fiction')
 
-# experience actions
-experience_actions_table = database.get_worksheet(1)
-experience_actions_df = pd.DataFrame(experience_actions_table.get_all_records()).iloc[: , :3]
-
-# mts
-mts_table = database.get_worksheet(2)
-mts_df = pd.DataFrame(mts_table.get_all_records()).iloc[: , :3]
-
-# pokedex
-pokedex_table = database.get_worksheet(3)
-pokedex_df = pd.DataFrame(pokedex_table.get_all_records()).iloc[: , :4]
-
-# students experience actions
-students_experience_actions_table = database.get_worksheet(4)
-students_experience_actions_df = pd.DataFrame(students_experience_actions_table.get_all_records()).iloc[: , :3]
-
-# students mts
-students_mts_table = database.get_worksheet(5)
-students_mts_df = pd.DataFrame(students_mts_table.get_all_records()).iloc[: , :2]
-
-# students pokedex
-students_pokedex_table = database.get_worksheet(6)
-students_pokedex_df = pd.DataFrame(students_pokedex_table.get_all_records()).iloc[: , :2]
-
-# Student details
-if profile == 'A0':
-    st.write('Perfil profe')
-
-    # query = '''
-    # select *
-    # from students_df
-    # left join students_experience_actions_df
-    # using(id_alumno)
-    # left join pokedex_df 
-    # using(id_pokemon)
-    # '''
-
-else:
-    ID_ALUMNO = profile
-
-    try:
-        student_df = pdsql.sqldf(
-            f'''
-            select * 
-            from students_df 
-            where id_alumno = "{ID_ALUMNO}"
-            '''
-        )
-        student_course = list(student_df.values[0])[1]
-        student_course_letter = list(student_df.values[0])[2]
-        student_name = list(student_df.values[0])[3]
-        student_surname = list(student_df.values[0])[4]
-        student_initial_points = list(student_df.values[0])[5]
-
-        student_pokemon_df = pdsql.sqldf(
-            f'''
-            select * 
-            from students_pokedex_df 
-            left join pokedex_df 
-            using(id_pokemon) 
-            where id_alumno = "{ID_ALUMNO}"
-            '''
-        )
-        student_pokemon = list(student_pokemon_df.values[0])[3]
-        student_pokemon_picture = list(student_pokemon_df.values[0])[4]
-
-        student_experience_actions_df = pdsql.sqldf(
-            f'''
-            select * 
-            from students_experience_actions_df 
-            where id_alumno = "{ID_ALUMNO}"
-            '''
-        )
-
-        student_points = list(pdsql.sqldf(
-            f'''
-            select sum(puntos_experiencia_accion) 
-            from student_experience_actions_df 
-            left join experience_actions_df 
-            using(id_accion) 
-            where id_alumno = "{ID_ALUMNO}"
-            '''
-        ).values[0])[0]
-
-        student_total_points = student_initial_points + student_points
-
-        # st.write(f'''
-        # {student_course}, 
-        # {student_course_letter}, 
-        # {student_name}, 
-        # {student_surname}, 
-        # {student_initial_points}, 
-        # {student_points}, 
-        # {student_total_points},
-        # {student_pokemon},
-        # {student_pokemon_picture}
-        # ''')
-
-        html = f'''
-        <html>
-
-        <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <style>
-        * {{box-sizing: border-box}}
-
-        .container {{
-        width: 100%;
-        background-color: #ddd;
-        }}
-
-        .skills {{
-        text-align: right;
-        padding-top: 10px;
-        padding-bottom: 10px;
-        color: white;
-        }}
-
-        .bold {{ 
-        font-weight: bold; 
-        font-size:1em; 
-        text-align:right; 
-        }} 
-        .button {{ 
-        text-align:center; 
-        }}
-
-        .experiencia {{width: {student_total_points}; background-color: #04AA6D;}}
-
-        .image {{
-        height: 250px;
-        width: auto;
-        }}
-
-        .image-container {{
-        text-align: center;
-        }}
-        </style>
-
-        </head>
-
-
-        <body>
-        <br> 
-        Alumno: {student_name} {student_surname}
-        <br> 
-        <br>
-        Curso: {student_course} {student_course_letter}
-        <br>
-        <br>
-        Pokemon:
-        <div class="image-container">
-        <img src={student_pokemon_picture} class="image"/>
-        </div>
-        <p>Experiencia:</p>
-        <div class="container">
-        <div class="skills experiencia">{student_total_points}</div>
-        </div>
-        <br> 
-        Medallas:
-        <br> 
-        <br>
-
-        </body>
-
-        </html>
-        '''
-        st.markdown(html, unsafe_allow_html=True)
+movie_df = pdsql.sqldf(
+    f'''
+    select 
+        fecha_limite,
+        poster_url,
+        pelicula,
+        año,
+        CC,
+        recomendador,
+        plataforma,
+        Adrián,
+        Alonso,
+        Ana,
+        Mamen,
+        (Adrián + Alonso + Ana + Mamen) / 4 as media
+    from movies_df
+    where nr != '' and pelicula != ''
+    '''
+)
     
+cols = st.columns(12)
+cols[0].markdown('**Fecha**')
+cols[1].markdown('**Poster**')
+cols[2].markdown('**Título**')
+cols[3].markdown('**Año**')
+cols[4].markdown('**País**')
+cols[5].markdown('**Recomendador**')
+cols[6].markdown('**Plataforma**')
+cols[7].markdown('**ADRIÁN**')
+cols[8].markdown('**ALONSO**')
+cols[9].markdown('**ANA**')
+cols[10].markdown('**MAMEN**')
+cols[11].markdown('**MEDIA**')
+
+for movie in movie_df.values:
+    movie_details = list(movie)
+
+    cols = st.columns(12)
+    cols[0].write(movie_details[0])
+    response = requests.get(movie_details[1])
+    cols[1].image(response.content, use_column_width='auto')
+    cols[2].write(movie_details[2])
+    cols[3].write(movie_details[3])
+    response = requests.get(f'https://countryflagsapi.com/png/{movie_details[4].lower()}')
+    cols[4].image(response.content, width=30)
+    cols[5].write(movie_details[5])
+    cols[6].write(movie_details[6])
+    try:
+        cols[7].write(int(movie_details[7]) / 100)
+        cols[8].write(int(movie_details[8]) / 100)
+        cols[9].write(int(movie_details[9]) / 100)
+        cols[10].write(int(movie_details[10]) / 100)
+        cols[11].write(int(movie_details[11]) / 100)
     except:
-        st.write('En desarrollo, gracias por su paciencia')
+        pass
+    
